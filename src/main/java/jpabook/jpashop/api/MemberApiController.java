93c2f12490cf867572395a2d05ed3e592d5c73cf
임영욱 <ywyim@gaia3d.com>
2025-04-15 18:02:34 +0900

    feat : 회원 등록 api 기능 구현

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller + @ResponseBody = @RestController
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    // api 스펙을 위한 DTO를 만들어라
+    // Entity 그대로 사용 X (parameter로 받지 말고 외부에 노출X)
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        memberService.join(member);
+
+        return new CreateMemberResponse(member.getId());
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
