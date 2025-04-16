5af1470016f61ea288018e19221326346b9fbee9
임영욱 <ywyim@gaia3d.com>
2025-04-16 12:09:59 +0900

    feat : 주문조회 api v2 기능 추가

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * XToOne
@@ -32,5 +40,34 @@ public class OrderSimpleApiController {
         }
 
         return all;
+    }
+
+    // lazy 로딩으로 인한 쿼리 호출이 많다
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        List<SimpleOrderDto> list = orders.stream()
+                .map(SimpleOrderDto::new)
+                .toList();
+
+        return list;
+    }
+
+    @Data
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    static class SimpleOrderDto {
+        Long orderId;
+        String name;
+        LocalDateTime orderDate;
+        OrderStatus orderStatus;
+        Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
